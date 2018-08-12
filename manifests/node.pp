5b5b89a5540b264d46a7d8e79d0a6ad4d2ceebfb
brijesh-git <34630713+brijesh-git@users.noreply.github.com>
2018-08-11 22:05:08 -0400

    node.pp
    --initial comit

---
+++ manifests/node.pp
@@ -0,0 +1,17 @@
+class dev_environment {
+
+  user { 'grace':
+    ensure      => present,
+    manage_home => true,
+    group       => ['wheel'],
+  }
+  package { 'vim' :
+    ensure => present,
+  }
+  file { '/home/grace/.vimrc':
+    ensure => file, 
+    source => 'puppet///modules/dev_environment/vimrc'
+  
+  }
+
+}
