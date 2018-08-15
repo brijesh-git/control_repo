e745fb7843db53f6c469d45f87a0056181e473e2
brijesh-git <34630713+brijesh-git@users.noreply.github.com>
2018-08-14 20:58:14 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,14 @@
 node default {
-  file{ '/root/README.md':
+ include my_dev_server
+}
+class my_dev_server {
+
+ file{ '/root/README.md':
     ensure => file,
     content => 'This is the readme file modified',
     owner => 'root',
+  }
+  package { "vim",
+    ensure => true
   }
 }
